47ba6ee5488b2911ebe60605ae8e9bc88b36c4f6
Kaileycar <kaicarbone@gmail.com>
2023-06-15 00:15:51 -0500

    added more visuals relating to the type of parking per city. Modified the csv file to include limit of 500 on each city

--- Project 1/config.py
+++ Project 1/config.py
@@ -1,2 +1,10 @@
 # Geoapify API Key
-geoapify_key = "eefb98014bc34193948d99ae46499e37"
+#Kailey
+geoapify_key = "83d88e9fc41b47379beb2445a0d747ce"
+#Peter
+#geoapify_key = "eefb98014bc34193948d99ae46499e37"
+
+
+
+
+
--- config.py
+++
@@ -1,2 +0,0 @@
-# Geoapify API Key
-geoapify_key = "eefb98014bc34193948d99ae46499e37"
